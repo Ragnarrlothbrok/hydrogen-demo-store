d3d04016c1fdf27b3e9bb3fd43704fee68fca89a
Juan P. Prieto <jp@calltheguys.co>
2025-06-23 11:18:33 -0700

    force dev

--- vite.config.ts
+++ vite.config.ts
@@ -4,45 +4,57 @@ import {oxygen} from '@shopify/mini-oxygen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [
-    hydrogen(),
-    oxygen(),
-    remix({
-      presets: [hydrogen.preset()],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        // v3_singleFetch: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-  mode: 'development',
-  resolve: {
-    conditions: ['development'],
-  },
-  ssr: {
-    optimizeDeps: {
-      include: ['typographic-base'],
-    },
-  },
-  optimizeDeps: {
-    include: [
-      'clsx',
-      '@headlessui/react',
-      'typographic-base',
-      'react-intersection-observer',
-      'react-use/esm/useScroll',
-      'react-use/esm/useDebounce',
-      'react-use/esm/useWindowScroll',
+export default defineConfig(({mode}) => {
+  console.log(`Vite mode: ${mode}`);
+  return {
+    plugins: [
+      hydrogen(),
+      oxygen(),
+      remix({
+        presets: [hydrogen.preset()],
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+          v3_lazyRouteDiscovery: true,
+          // v3_singleFetch: true,
+        },
+      }),
+      tsconfigPaths(),
     ],
-  },
-  build: {
-    // Allow a strict Content-Security-Policy
-    // withtout inlining assets as base64:
-    assetsInlineLimit: 0,
-  },
+    resolve: {
+      alias: [
+        {
+          find: '@shopify/hydrogen',
+          replacement:
+            './node_modules/@shopify/hydrogen/dist/development/index.js',
+        },
+      ],
+      conditions:
+        mode === 'production'
+          ? ['development', 'module', 'import', 'default'] // Force development even in prod
+          : ['development', 'module', 'import', 'default'],
+    },
+    ssr: {
+      optimizeDeps: {
+        include: ['typographic-base'],
+      },
+    },
+    optimizeDeps: {
+      include: [
+        'clsx',
+        '@headlessui/react',
+        'typographic-base',
+        'react-intersection-observer',
+        'react-use/esm/useScroll',
+        'react-use/esm/useDebounce',
+        'react-use/esm/useWindowScroll',
+      ],
+    },
+    build: {
+      // Allow a strict Content-Security-Policy
+      // withtout inlining assets as base64:
+      assetsInlineLimit: 0,
+    },
+  };
 });
