991ca24d13a512e1a4d74010b4bb8cd5c82e7da2
Apoorv Tiwari <apurv9950@gmail.com>
2025-08-13 18:25:04 +0530

    Update SmartcodeTest.tsx

--- app/components/SmartcodeTest.tsx
+++ app/components/SmartcodeTest.tsx
@@ -6,14 +6,7 @@ export default function SmartcodeTest() {
   const r = `<script>console.log('FAIL: dangerouslySetInnerHTML — should NOT run');</script>`;
 
   useEffect(() => {
-    // ✅ Working: create a real <script> node and append it
-    const ok = document.createElement('script');
-    ok.id = 'demo-ok-script';
-    ok.text = `console.log('OK: createElement + appendChild executed');`;
-    document.body.appendChild(ok);
-    hydrated = true;
-
-    //Solution 1: Alternatively add SmartCode from useEffect
+    // Alternatively add SmartCode from useEffect [Not recommended due to possible flicker issues]
     // const script = document.createElement('script');
     // script.id = 'demo-ok-script';
     // script.text = r; //SmartCode as text
@@ -21,16 +14,16 @@ export default function SmartcodeTest() {
   }, []);
 
   //Failing implementation.
-  return hydrated ? (
-    <>
-      <link href="https://dev.visualwebsiteoptimizer.com" rel="preconnect" />
-      <div id="vwo-code" dangerouslySetInnerHTML={{__html: r}} />
-    </>
-  ) : null;
+  // return hydrated ? (
+  //   <>
+  //     <link href="https://dev.visualwebsiteoptimizer.com" rel="preconnect" />
+  //     <div id="vwo-code" dangerouslySetInnerHTML={{__html: r}} />
+  //   </>
+  // ) : null;
 
-  //Solution 2: Directly add the SmartCode without any dependency.
-  // return <>
-  //   <link href="https://dev.visualwebsiteoptimizer.com" rel="preconnect" />
-  //   <div id="vwo-code" dangerouslySetInnerHTML={{__html: r}} />
-  // </>;
+  //Solution: Directly add the SmartCode without any dependency.
+  return <>
+    <link href="https://dev.visualwebsiteoptimizer.com" rel="preconnect" />
+    <div id="vwo-code" dangerouslySetInnerHTML={{__html: r}} />
+  </>;
 }
